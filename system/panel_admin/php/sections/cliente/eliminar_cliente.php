effc8c7ab9ed0afd6952d60609e674413d41f24d
ROCÍO\Rocio <rociocellini@gmail.com>
2017-12-19 14:18:43 -0300

    New file "eliminar_cliente.php"

---
+++ system/panel_admin/php/sections/cliente/eliminar_cliente.php
@@ -0,0 +1,51 @@
+<?php
+//header('X-Frame-Options: SAMEORIGIN');
+//header('X-Content-Type-Options: nosniff');
+//header('X-XSS-Protection: 1;mode=block');
+//header("Content-type: text/plain");	
+session_start();
+//if(isset($_SESSION['Id_Usuario'])) {
+
+$json = file_get_contents('php://input');
+$data=json_decode($json);
+
+
+//$type_accion=$data->{'type_accion'};
+
+
+$type_accion='eliminar_cliente';
+
+if($type_accion==="eliminar_cliente"){
+
+//************************************************************************************************//	
+	include "../../conexion.php";	
+
+	$id_cliente=2;
+
+	$sql='DELETE FROM cliente WHERE id_cliente=?';
+	//die($sql);
+
+	$stmt_delete = $conn->prepare($sql);
+	if($stmt_delete === false) {
+	trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+	}
+
+
+	$stmt_delete->bind_param('i',$id_cliente);
+	$stmt_delete->execute();
+
+	//echo $stmt->affected_rows;
+	$stmt_delete->close();
+
+	$message="El cliente ha sido eliminado correctamente.";
+
+//***************************************************************************************///
+
+  $item=array('Message' => utf8_encode($message));
+  $json = json_encode($item);
+  echo $json;
+            
+   } //if($type_accion==="eliminar_cliente")
+   //agregar un json con el error si no se guardó en la BD
+
+?>      
