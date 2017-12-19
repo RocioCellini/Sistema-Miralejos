82c6a1c7fff55a8496eb20242efde5863fbd188b
ROCÍO\Rocio <rociocellini@gmail.com>
2017-12-19 14:53:31 -0300

    New file "eliminar_planta.php"

---
+++ system/panel_admin/php/sections/planta/eliminar_planta.php
@@ -0,0 +1,50 @@
+<?php
+//header('X-Frame-Options: SAMEORIGIN');
+//header('X-Content-Type-Options: nosniff');
+//header('X-XSS-Protection: 1;mode=block');
+//header("Content-type: text/plain");	
+session_start();
+//if(isset($_SESSION['Id_Usuario'])) {
+
+$json = file_get_contents('php://input');
+$data=json_decode($json);
+
+
+//$type_accion=$data->{'type_accion'};
+
+
+$type_accion='eliminar_planta';
+
+if($type_accion==="eliminar_planta"){
+
+//************************************************************************************************//	
+	include "../../conexion.php";	
+
+	$id_planta=3;
+
+	$sql='DELETE FROM planta WHERE id_planta=?';
+
+	$stmt_delete = $conn->prepare($sql);
+	if($stmt_delete === false) {
+	trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+	}
+
+
+	$stmt_delete->bind_param('i',$id_planta);
+	$stmt_delete->execute();
+
+	//echo $stmt->affected_rows;
+	$stmt_delete->close();
+
+	$message="La planta ha sido eliminado correctamente.";
+
+//***************************************************************************************///
+
+  $item=array('Message' => utf8_encode($message));
+  $json = json_encode($item);
+  echo $json;
+            
+   } //if($type_accion==="eliminar_planta")
+   //agregar un json con el error si no se guardó en la BD
+
+?>      
