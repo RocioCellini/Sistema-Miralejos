51210fefbcfdb36d933ee72e052c5030bd14b43a
ROCÍO\Rocio <rociocellini@gmail.com>
2017-12-19 14:56:50 -0300

    New file "eliminar_tabla_intermedia_dpto.php"

---
+++ system/panel_admin/php/sections/tabla_intermedia_dpto/eliminar_tabla_intermedia_dpto.php
@@ -0,0 +1,50 @@
+<?php
+//header('X-Frame-Options: SAMEORIGIN');
+//header('X-Content-Type-Options: nosniff');
+//header('X-XSS-Protection: 1;mode=block');
+//header("Content-type: text/plain");	
+session_start();
+//if(isset($_SESSION['Id_Usuario'])) {
+
+$json = file_get_contents('php://input');
+$data=json_decode($json);
+
+
+//$type_accion=$data->{'type_accion'};
+
+
+$type_accion='eliminar_ti_dpto';
+
+if($type_accion==="eliminar_ti_dpto"){
+
+//************************************************************************************************//	
+	include "../../conexion.php";	
+
+	$id_tabla=3;
+
+	$sql='DELETE FROM tabla_intermedia_dpto WHERE id_tabla=?';
+
+	$stmt_delete = $conn->prepare($sql);
+	if($stmt_delete === false) {
+	trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+	}
+
+
+	$stmt_delete->bind_param('i',$id_tabla);
+	$stmt_delete->execute();
+
+	//echo $stmt->affected_rows;
+	$stmt_delete->close();
+
+	$message="La tabla_intermedia_dpto ha sido eliminado correctamente.";
+
+//***************************************************************************************///
+
+  $item=array('Message' => utf8_encode($message));
+  $json = json_encode($item);
+  echo $json;
+            
+   } //if($type_accion==="eliminar_ti_dpto")
+   //agregar un json con el error si no se guardó en la BD
+
+?>      
