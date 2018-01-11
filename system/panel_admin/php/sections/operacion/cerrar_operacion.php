1d0a5de1acc7743d95cf58292363f4b76945795c
ROCÍO\Rocio <rociocellini@gmail.com>
2018-01-11 11:52:13 -0300

    Fix cerrar_operacion.php

--- system/panel_admin/php/sections/operacion/cerrar_operacion.php
+++ system/panel_admin/php/sections/operacion/cerrar_operacion.php
@@ -9,32 +9,23 @@ session_start();
 $json = file_get_contents('php://input');
 $data=json_decode($json);
 
+$type_accion=$data->{'type_accion'};
 
-//$type_accion=$data->{'type_accion'};
-$type_accion='nueva_operacion';
-
-if($type_accion==="nueva_operacion"){
+if($type_accion==="cerrar_operacion"){
 
 //************************************************************************************************//	
 	include "../../conexion.php";	
-	/*
-	$operationid =$data->{'Id_Operacion'};
-	$idinscripto =$data->{'Id_Inscripto'};
-	$status_pago =$data->{'Pago'};
-  $detalle =$data->{'Detalle'};
-	*/
-  $id_cliente=2;
-  $id_vendedor=22;
-  $fecha_ultimo_llamado='2011-03-14';
-  $cant_llamados=5;
-  $fecha_cierre_operacion='2011-03-16';
-  $hora_cierre_operacion='17:00:01';
-  $id_edificio=4;
-  $id_planta=3;
-  $id_dpto=2;
-  $observaciones='todo tranquilo';
-
-  $item="";
+	
+	$id_cliente =$data->{'id_cliente'};
+	$id_vendedor =$data->{'id_vendedor'};
+	$fecha_ultimo_llamado =$data->{'fecha_ultimo_llamado'};
+  $cant_llamados =$data->{'cant_llamados'};
+  $fecha_cierre_operacion =$data->{'fecha_cierre_operacion'};
+  $hora_cierre_operacion =$data->{'hora_cierre_operacion'};
+  $id_edificio =$data->{'id_edificio'};
+  $id_planta =$data->{'id_planta'};
+  $id_dpto =$data->{'id_dpto'};
+  $observaciones =$data->{'observaciones'}; 
 
   $sql_insert='INSERT INTO operacion (id_operacion, id_cliente, id_vendedor, fecha_ultimo_llamado, cant_llamados, fecha_cierre_operacion, hora_cierre_operacion, id_edificio, id_planta, id_dpto, observaciones) VALUES (?,?,?,?,?,?,?,?,?,?,?)';
 
@@ -51,21 +42,18 @@ if($type_accion==="nueva_operacion"){
 
   $last_id=mysqli_insert_id($conn);
 
-  echo $last_id;
-
   if($last_id!=0){
     $message="Se guardo una nueva operacion";
   }else{
     $message="La nueva operacion no se guardó";
-  }//en este caso el msg no se muestra bien
+  }
 
   //***************************************************************************************///
 
-  $item=array('Message' => utf8_encode($message));
+  $item=array('Mensaje' => utf8_encode($message));
   $json = json_encode($item);
   echo $json;
             
    } //if($type_accion==="nueva_operacion")
-   //agregar un json con el error si no se guardó en la BD
 
 ?>      
