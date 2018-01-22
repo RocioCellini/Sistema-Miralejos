8d41cfc1b4d214e7c3ad3bbabc00d4115d8ac60d
ROCÍO\Rocio <rociocellini@gmail.com>
2018-01-22 16:09:34 -0300

    Organized app.js

--- system/panel_admin/js/app.js
+++ system/panel_admin/js/app.js
@@ -1,13 +1,13 @@
 (function(_angular) {
 
-      "use strict";
+  "use strict";
 
-      var app = _angular.module("GestionVentas", ["ngSanitize", "ngAnimate", 
-      "ngTable", "ui.router", "ui.bootstrap"]);
+  var app = _angular.module("GestionVentas", ["ngSanitize", "ngAnimate", 
+            "ngTable", "ui.router", "ui.bootstrap"]);
 
 
-      app.config(function($stateProvider, $urlRouterProvider){ 
-      // $urlRouterProvider.otherwise('/home');
+  app.config(function($stateProvider, $urlRouterProvider){ 
+  // $urlRouterProvider.otherwise('/home');
 
       $urlRouterProvider.rule(function ($injector, $location) {
 
@@ -28,22 +28,34 @@
       });
 
 
-    $stateProvider.state('GestionVentas', {
-      views: {
-        'content': {
-         template:'<div ui-view></div>'
-        },
-        'footer': {
-          templateUrl: 'templates/footer.html',
-          controller: 'FooterController as vf'
+      $stateProvider.state('GestionVentas', {
+        views: {
+          'content': {
+           template:'<div ui-view></div>'
+          },
+          'footer': {
+            templateUrl: 'templates/footer.html',
+            controller: 'FooterController as vf'
+          }
         }
-      }
-    }) 
+      }) 
 
-     .state('GestionVentas.index',{
-        url:'/NuevaRelacion',
-        templateUrl:'templates/nueva_relacion.html',
-        controller: 'NuevaRelacion as $ctrl_nr'
+      .state('GestionVentas.index',{
+        url:'/NuevoCliente',
+        templateUrl:'templates/nuevo_cliente.html',
+        controller: 'NuevoCliente as $ctrl_nc'
+      })
+
+      .state('GestionVentas.nuevoDpto',{
+        url:'/NuevoDpto',
+        templateUrl:'templates/nuevo_dpto.html',
+        controller: 'NuevoDpto as $ctrl_nd'
+      })
+
+      .state('GestionVentas.nuevoEdificio',{
+        url:'/NuevoEdificio',
+        templateUrl:'templates/nuevo_edificio.html',
+        controller: 'NuevoEdificio as $ctrl_ne'
       })
 
       .state('GestionVentas.nuevoLlamado',{
@@ -52,84 +64,37 @@
         controller: 'NuevoLlamado as $ctrl_nl'
       })
 
+      .state('GestionVentas.nuevaLocalidad',{
+        url:'/NuevaLocalidad',
+        templateUrl:'templates/nueva_localidad.html',
+        controller: 'NuevaLocalidad as $ctrl_nloc'
+      })
+
       .state('GestionVentas.cerrarOperacion',{
         url:'/CerrarOperacion',
         templateUrl:'templates/cerrar_operacion.html',
         controller: 'CerrarOperacion as $ctrl_co'
       })
 
-/*
-    .state('GestionVentas.index',{
-        url:'/NuevaRelacion',
-        templateUrl:'templates/nueva_relacion.html',
-        controller: 'NuevaRelacion as $ctrl_nr'
-      })
-
-
-
-    .state('GestionVentas.index',{
-        url:'/NuevoCliente',
-        templateUrl:'templates/nuevo_cliente.html',
-        controller: 'NuevoCliente as $ctrl_nc'
-      })
-
-    .state('GestionVentas.index',{
-        url:'/NuevoLlamado',
-        templateUrl:'templates/nuevo_llamado.html',
-        controller: 'NuevoLlamado as $ctrl_nl'
-      })
-
-     .state('GestionVentas.index',{
-        url:'/NuevoCliente',
-        templateUrl:'templates/nuevo_cliente.html',
-        controller: 'NuevoCliente as $ctrl_nc'
-      })
-
-    .state('GestionVentas.index',{
+      .state('GestionVentas.nuevaPlanta',{
         url:'/NuevaPlanta',
         templateUrl:'templates/nueva_planta.html',
         controller: 'NuevaPlanta as $ctrl_np'
       })
 
-
-    .state('GestionVentas.index',{
-        url:'/CerrarOperacion',
-        templateUrl:'templates/cerrar_operacion.html',
-        controller: 'CerrarOperacion as $ctrl_co'
+      .state('GestionVentas.nuevaRelacion',{
+        url:'/NuevaRelacion',
+        templateUrl:'templates/nueva_relacion.html',
+        controller: 'NuevaRelacion as $ctrl_nr'
       })
 
-
-    .state('GestionVentas.index',{
-        url:'/NuevoEdificio',
-        templateUrl:'templates/nuevo_edificio.html',
-        controller: 'NuevoEdificio as $ctrl_ne'
-      })
- 
-     .state('GestionVentas.index',{
-        url:'/NuevaLocalidad',
-        templateUrl:'templates/nueva_localidad.html',
-        controller: 'NuevaLocalidad as $ctrl_nloc'
-      })
-      .state('GestionVentas.index',{
-        url:'/NuevoDpto',
-        templateUrl:'templates/nuevo_dpto.html',
-        controller: 'NuevoDpto as $ctrl_nd'
-      })
-     .state('GestionVentas.index',{
+     .state('GestionVentas.nuevoVendedor',{
         url:'/NuevoVendedor',
         templateUrl:'templates/nuevo_vendedor.html',
         controller: 'NuevoVendedor as $ctrl_nv'
-      })    
-     .state('GestionVentas.index',{
-        url:'/NuevoLlamado',
-        templateUrl:'templates/nuevo_llamado.html',
-        controller: 'NuevoLlamado as $ctrl_nl'
-      })
-      .state('GestionVentas.index',{
-        url:'/NuevoCliente',
-        templateUrl:'templates/nuevo_cliente.html',
-        controller: 'NuevoCliente as $ctrl_nc'
-      })
+      })  
+
+      /*  
       .state('GestionVentas.editarCliente',{
         url:'/EditarCliente',
         templateUrl:'templates/editar_cliente.html',
@@ -139,18 +104,8 @@
         url:'/EliminarCliente',
         templateUrl:'templates/eliminar_cliente.html',
         controller: 'EliminarCliente as $ctrl_elimc'
-      })
-      
-      .state('GestionVentas',{
-        url:'/NuevoDpto',
-        templateUrl:'templates/nuevo_dpto.html',
-        controller: 'NuevoDpto as $ctrl_nd'
-      })
-
-       url:'/NuevoCliente',
-        templateUrl:'templates/nuevo_cliente.html',
-        controller: 'NuevoCliente as $ctrl_nc'
-      */ ;
+      })      
+      */
 
       }).run(function($state) {
         
