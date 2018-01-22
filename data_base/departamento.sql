7dcb9066f2b5a17a17555c9a57dbc9ba725cccb5
ROCÍO\Rocio <rociocellini@gmail.com>
2018-01-22 16:21:54 -0300

    Actualized table "departamento"

--- data_base/departamento.sql
+++ data_base/departamento.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-01-2018 a las 12:34:26
+-- Tiempo de generación: 22-01-2018 a las 19:20:54
 -- Versión del servidor: 5.7.14
 -- Versión de PHP: 5.6.25
 
@@ -36,13 +36,52 @@ CREATE TABLE `departamento` (
 --
 
 INSERT INTO `departamento` (`id_dpto`, `nombre`) VALUES
-(1, '4'),
-(2, '4'),
-(3, '2'),
-(5, '0'),
-(6, '0'),
-(7, 'A6'),
-(8, 'B5');
+(1, 'A'),
+(2, 'B'),
+(3, 'C'),
+(4, 'D'),
+(5, 'E'),
+(6, 'F'),
+(7, 'G'),
+(8, 'H'),
+(9, 'I'),
+(10, 'J'),
+(11, 'K'),
+(12, 'L'),
+(13, 'M'),
+(14, 'N'),
+(15, 'O'),
+(16, 'P'),
+(17, 'Q'),
+(18, 'R'),
+(19, 'S'),
+(20, 'T'),
+(21, 'U'),
+(22, 'V'),
+(23, 'W'),
+(24, 'X'),
+(25, 'Y'),
+(26, 'Z'),
+(27, 'LL'),
+(28, 'ñ'),
+(29, 'Local 1'),
+(30, 'Local 2'),
+(31, 'Local 3'),
+(32, 'Local A'),
+(33, 'Local B'),
+(34, 'Local C'),
+(35, 'Local D'),
+(36, 'Local E'),
+(37, 'Local F'),
+(38, 'Local G'),
+(39, 'Local 4'),
+(40, 'Local 5'),
+(41, 'Local 6'),
+(42, 'Local 7'),
+(43, 'Local 8'),
+(44, 'Local 9'),
+(45, 'Local 10'),
+(46, 'Local 11');
 
 --
 -- Índices para tablas volcadas
@@ -62,7 +101,7 @@ ALTER TABLE `departamento`
 -- AUTO_INCREMENT de la tabla `departamento`
 --
 ALTER TABLE `departamento`
-  MODIFY `id_dpto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_dpto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
