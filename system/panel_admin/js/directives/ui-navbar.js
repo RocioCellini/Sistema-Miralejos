ec0605c7685918984de5ddd7186c7baccbe41bb5
DESKTOP-QDHB8U5\feder <federicovongromann@gmail.com>
2018-01-25 17:18:46 +0100

    Corrected syntax in navbar

--- system/panel_admin/js/directives/ui-navbar.js
+++ system/panel_admin/js/directives/ui-navbar.js
@@ -74,7 +74,7 @@ angular.module('ui.navbar', [
 angular.module("template/navbar-li.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/navbar-li.html",
     "<li ng-class=\"{divider: leaf.name == 'divider'}\">\n" +
-    "    <a href=\"{{leaf.link}}\" ng-if=\"leaf.name !== 'divider'\">{{leaf.name}}</a>\n" +
+    "    <a ui-sref=\"{{leaf.link}}\" ng-if=\"leaf.name !== 'divider'\">{{leaf.name}}</a>\n" +
     "</li>");
 }]);
 
